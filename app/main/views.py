97e647257e3e75f678d9e2ce616a68252c731c00
ThiraTheNerd <thiragithinji@gmail.com>
2021-06-09 02:37:59 +0300

    add all the views

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,6 @@
 from flask import Flask,render_template,redirect,url_for,request
 from . import main
-from ..request import get_news, get_source,news_search
+from ..request import get_news,get_source,news_search
 from ..models import Article
 
 #Views
